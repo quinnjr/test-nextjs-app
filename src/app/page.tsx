a9b94cb635362b2276030cea7f23bf9510c9d4f1
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2024-01-25 15:42:04 -0500

    Adding limit to database query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,46 @@
+'use client'
+
+import type { User } from '@prisma/client';
 import React, { useEffect, useState } from 'react';
 import { Table } from 'flowbite-react';
 
-import ServerSideUsersTable from '@/components/ServerSideUsersTable';
-
 export default function Home() {
-  // const [users, setUsers]: [any[], any] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [limit, setLimit] = useState<number>(10)
 
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     const response = await fetch('/api/users');
-  //     const data = await response.json();
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(`/api/users?limit=${limit}`);
+      const data = await response.json();
 
-  //     setUsers(data);
-  //   }
+      setUsers(data);
+    }
 
-  //   fetchUsers();
-  // }, []);
+    fetchUsers();
+  }, [limit]);
 
   return (
-  //  <Table>
-  //   <Table.Head>
-  //     <Table.HeadCell>ID</Table.HeadCell>
-  //     <Table.HeadCell>First Name</Table.HeadCell>
-  //     <Table.HeadCell>Last Name</Table.HeadCell>
-  //   </Table.Head>
-  //   <Table.Body>
-  //     {users.map((user: any) => (
-  //         <Table.Row key={user.id}>
-  //           <Table.Cell>{user.id}</Table.Cell>
-  //           <Table.Cell>{user.firstName}</Table.Cell>
-  //           <Table.Cell>{user.lastName}</Table.Cell>
-  //         </Table.Row>
-  //     ))}
-  //   </Table.Body>
-  //  </Table>
     <div>
-      <ServerSideUsersTable />
+      <Table>
+        <Table.Head>
+          <Table.HeadCell>ID</Table.HeadCell>
+          <Table.HeadCell>First Name</Table.HeadCell>
+          <Table.HeadCell>Last Name</Table.HeadCell>
+        </Table.Head>
+        <Table.Body>
+          {users.map((user: any) => (
+              <Table.Row key={user.id}>
+                <Table.Cell>{user.id}</Table.Cell>
+                <Table.Cell>{user.firstName}</Table.Cell>
+                <Table.Cell>{user.lastName}</Table.Cell>
+              </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+      <div>
+        <label htmlFor="limit">Limit: </label>
+        <input type="number" id="limit" value={limit} onChange={(e) => setLimit(parseInt(e.target.value))} />
+      </div>
     </div>
   );
 }
--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,12 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { prisma} from '../../services/db';
 
-const users = async (req: any, res: any) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const users = await prisma.user.findMany();
+    const { limit } = req.query;
+    const users = await prisma.user.findMany({
+      take: parseInt(limit as string) || 100
+    });
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json({error: err});
   }
 }
 
-export default users;
+export default handler;
