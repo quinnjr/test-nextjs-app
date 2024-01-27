feb8098231e6a6b5befc482741e268e140a9522a
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2024-01-27 12:53:10 -0500

    Adding initial Angular work

--- test-angular-ssr/src/app/app.component.ts
+++ test-angular-ssr/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { User } from '@prisma/client';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'test-nest-fastify';
+export class AppComponent implements OnInit {
+  public title = 'test-angular-ssr';
+  public users?: Observable<User[]>;
+
+  constructor(private readonly http: HttpClient) {}
+
+  public ngOnInit() {
+    initFlowbite();
+
+    this.users = this.http.get<User[]>('http://localhost:3000/api/users');
+  }
 }
